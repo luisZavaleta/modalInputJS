a0d33083a8408d7e74dbb274d3c5f9368ef1063d
LuisZavaleta <luixZavaleta@gmail.com>
2015-08-17 00:17:28 -0500

    Begin

---
+++ js/modalInput.js
@@ -0,0 +1,195 @@
+
+
+/**
+ * Name: 			modalInput.js
+ * 
+ * Description:		Creates and configure a modal window dinamicaly.
+ * 
+ */
+
+var minHtml = '<div id="modalBaseContainer"><div id="%modalInputId%" class="modal %vulcanoModalFade%" role="dialog" style=""> <div class="modal-dialog %vulcanoModalSize%"> <!-- Modal content--> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" style="color: red;">&times;</button> <h4 class="modal-title">%vulcanoModalHeader%</h4> </div> <div class="modal-body"> <div class="container-fluid"> %vulcanoModalHtml% </div> </div> <div class="modal-footer"> <button type="button" class="btn btn-primary saveButton" data-dismiss="modal">%vulcanoModalSave%</button> <button type="button" class="btn btn-warning closeButton" data-dismiss="modal">%vulcanoModalClose%</button> </div> </div> </div></div></div>';
+
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+//var minHtml = '';
+
+	
+	
+	
+	
+	var modalInput ={};
+
+
+ 
+var modalSizeLarge =["lg", "large", "modal-lg"];  
+var modalSizeSmall =["sm", "small", "modal-sm"]; 
+
+//Medium values not included in modalSizes, because everithing that is not large or small will be treated ad medium.
+var modalSizes = modalSizeSmall.concat(modalSizeLarge); 
+
+
+
+/**
+ *  ============ CREATE THE MODAL AND SET THE ELEMENT THAT WILL TRIGER IT =========
+ * 
+ * @param selector: 	Jquery Selector of the element that will triger the modal window. default: modalButton.
+ * @param parent: 		Parent Element thet contains the selector.
+ * @param html: 		Html that will wi put in the body of the modal.
+ * @param event: 		Event that will triger the modal, default "click"
+ * @param size: 		Size o the modal, can be: lg, large, modal-lg, md, medium, modal-md, sm, small, modal-sm: default: md.
+ * @param fade: 		Boolean, If false, modal just appears and dissapears.
+ * @param options: 		JSON with modal options.
+ * @param headerText: 	Text to be set in the header.
+ * @param closeLabel: 	Text for the close button, default Cerrar.
+ * @param saveLabel:    Text for save button, default Guardar.
+ * @param saveOnClose:  Boolean, thar indicates if we should close the modal if the user decide to save, default true. 
+ */
+
+
+
+
+
+modalInput.createModal = function(params){
+	
+	
+	
+	//Set hidden html to the document.	
+	
+	if(!$("#modalInputId")[0]){
+		$("html").append(minHtml);
+	}
+	
+	params = modalInput.setDefaultValues(params);	
+	modalInput.openModal(params); 
+	
+	$("html").append(modalInput.getModalHtml(params));
+	
+	
+	
+};
+
+
+modalInput.setDefaultValues = function(params){
+	
+	if(!params){
+		params = {};
+	}
+	
+	if(!params.selector){
+		params.selector = ".modalButton";
+	}
+	
+	if(!params.event){
+		params.event = "click";
+	}
+	
+	if(!params.fade || params.fade){
+		params.fade = "fade";
+	}
+	
+	if(!params.options){
+		params.options = {};
+	}
+	
+	if(!params.closeLabel){
+		params.closeLabel = "Cerrar";
+	}
+	
+	if(!params.saveLabel){
+		params.saveLabel = "Guardar";
+	}
+	
+	if(!params.headerText){
+		params.headerText = "Vulcano Modal Input";
+	}
+	
+	if(!params.html){
+		params.html = "<h1>Hola Mundo</h1>";
+	}
+	
+	
+	
+	params.size = modalInput.getModalSize(params.size);
+	
+	return params;
+	
+};
+
+modalInput.openModal = function(params){
+	
+console.log(params);
+var modalTrigger = null;
+
+
+	if(!!params.parent){	
+		modalTrigger = $(params.parent).children(params.selector);
+	}else{
+		modalTrigger = $(params.selector);
+	}
+	
+	modalTrigger.on(params.event,function(){
+		$("#modalInputId").modal();
+	});
+	
+};
+
+
+
+modalInput.getModalSize = function(modalSize){
+	
+	var sizeString ="";	
+	if(!!modalSize && !modalSizes.contains(modalSize, true)){	
+		if(modalSizeLarge.contains(modalSize, true)){
+			sizeString = "modal-lg";
+		}else{
+			sizeString = "modal-sm";
+		}		
+	}
+	
+	return sizeString;
+};
+
+
+modalInput.getModalHtml = function(params){
+	
+	var modalBaseHtml = $("#modalBaseContainer").html(); 
+	
+	modalBaseHtml = vulcanoUtil.template(modalBaseHtml, {	
+		
+		modalInputId : "modalInputId",
+		vulcanoModalFade: params.fade,
+		vulcanoModalSize: params.size,
+		vulcanoModalHeader: params.headerText,
+		vulcanoModalHtml: params.html,
+		vulcanoModalSave: params.saveLabel,
+		vulcanoModalClose: params.closeLabel
+		
+	});
+	
+	
+	return modalBaseHtml;
+	
+};
+
+
+
+
+
+
+
+
+
+
+var params = {};
+
+
---
+++ js/vulcano-not-util-yet.js
@@ -0,0 +1,180 @@
+var vulcanoUtil = {};
+
+vulcanoUtil.clone = function clone(obj) {
+	return jQuery.extend(true, {}, obj);
+};
+
+/**
+ * Get information from a HTML page, it could get the value of an input or the html of a
+ * contenteditable element.
+ * 
+ * @selector: Jquery selector of the element
+ * @type: {value, html}
+ */
+vulcanoUtil.getTextFromDOM = function(selector, type) {
+
+	switch (type) {
+		case "value":
+		case "val":
+			return $(selector).val();
+			break;
+		case "html":
+			return $(selector).html();
+			break;
+		default:
+			throw "IllegalArgumentException---->" + type;
+
+	}
+	;
+
+};
+
+/**
+ * Used to get the data (var or html) of a collection of elements, for example to iterate over tr in
+ * a table and get all the tds (is needed to provide the td selectors).
+ * 
+ * @collectionSelector: jquery selector of the container that will be iterated.
+ * @childSelectors: Array with the name and type of the individual elements that will be getted. in
+ *                  the following format: [{ "selector": "7", "type": "7", "name": "nombre" }, {
+ *                  "selector": "7", "type": "7", "name": "nombre" }]
+ */
+vulcanoUtil.getCollectionFromDOM = function(collectionSelector, childsParams) {
+
+	if (collectionSelector == null || childsParams == null || jQuery.type(childsParams) != "array"
+			|| childsParams.length < 1) {
+
+		throw "getCollectionFromDOM - IllegalArgumentException ";
+	}
+
+	var returnArray = [];
+	var elementsToIterate = $(collectionSelector);
+
+	$.each(elementsToIterate, function(index, singleElement) {
+
+		var childsRet = {};
+
+		$.each(childsParams, function(indxChildSelector, childSelector) {
+
+			var domElement = $(singleElement).find(childSelector.selector);
+
+			childsRet[childSelector.name] = vulcanoUtil.getTextFromDOM(domElement, childSelector.type);
+
+		});
+		returnArray.push(childsRet);
+
+	});
+
+	return returnArray;
+
+};
+
+/**
+ * Used to get html or values of different elements in a webpage.
+ * 
+ * @params: json with all the information with the following pattern: [ { "selector" : ".divTest",
+ *          "type" : "html", "name" : "test1" }, { "selector" : "tr", "name" : "tabla", "data" : [ {
+ *          "selector" : ".hola", "type" : "html", "name" : "hola" }, { "selector" : ".adios",
+ *          "type" : "html", "name" : "adios" }, { "selector" : "td input", "type" : "val", "name" :
+ *          "ok" } ], "type" : "collection" }, { "selector" : ".divTest2 input", "type" : "val",
+ *          "name" : "test2" } ];
+ * @jsonExample: http://www.jsoneditoronline.org/?id=931b13e0e98649af2d73709c860603db
+ */
+vulcanoUtil.getDataFromForm = function(elements) {
+
+	var data = {};
+
+	$.each(elements, function(index, singleElement) {
+
+		if (singleElement.type == "collection") {
+			data[singleElement.name] = vulcanoUtil.getCollectionFromDOM(singleElement.selector, singleElement.data);
+		} else {
+			data[singleElement.name] = vulcanoUtil.getTextFromDOM(singleElement.selector, singleElement.type);
+		}
+
+	});
+
+	return data;
+};
+
+/**
+ * Used to verify if a jquery object really exists, for example $('#notAnElement') returns [object
+ * Object], but is an object with 0 elements.
+ */
+$.fn.exists = function() {
+	return this.length !== 0;
+};
+
+/**
+ * @param templateid: html of the element that will be used as template. example: <br>
+ *        <code>
+ *      	<div id="mytemplate">
+ *				<p>%test%</p>
+ *				<p>%word%</p>
+ *			</div>
+ *  	</code>
+ * @param data
+ * @returns
+ * @xample document.getElementById("my").innerHTML=template("mytemplate",{test:"MYTEST",word:"MYWORD"})
+ * @source: (based in) http://stackoverflow.com/a/378001/597786
+ */
+vulcanoUtil.template = function(templateid, data) {
+	
+	console.log("templateid======>"+templateid);
+	return templateid.replace(/%(\w*)%/g, function(m, key) {
+		return data.hasOwnProperty(key) ? data[key] : '%' + key + '%';
+	});
+};
+
+
+
+vulcanoUtil.undefinedOrFalse  = function(objt){
+	return (objt == undefined || !objt);
+};
+
+
+
+
+//replace for JSON.toCssString in vulcanoUtils
+
+JSON.toCssStringNew = function(jsonObject) {
+	
+	if(vulcanoUtil.undefinedOrFalse(jsonObject)){
+		return "";
+	}
+	
+	var cssString = "";
+
+	for (variable in jsonObject) {
+		if (jsonObject.hasOwnProperty(variable)) {
+			cssString += variable + ":" + jsonObject[variable] + ";";
+		}
+	}
+	return cssString;
+};
+
+JSON.htmlStringify = function(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+};
+
+
+
+
+
